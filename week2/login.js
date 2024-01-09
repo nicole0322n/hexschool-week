8e8af3cfb6d5dde40fba04bf26155c262d9ea6f7
nicole0322n <nicole0322n@gamil.com>
2024-01-09 16:22:16 +0800

    改vue方式串API

--- week2/login.js
+++ week2/login.js
@@ -1,32 +1,38 @@
-// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
-const url = 'https://vue3-course-api.hexschool.io/v2'; // 請加入站點
-const path = 'haru'; // 請加入個人 API Path
+createApp({
+    data(){
+        return{
+            user :{
+                username: '',
+                password: '',
+            },
+        }
+    },
+    methods:{
+        login(){
+            const url = 'https://vue3-course-api.hexschool.io/v2'; // 加入站點
+                         
+            const path = 'haru'; // 請加入個人 API Path
+            
+            axios.post(`${url}/admin/signin`, this.user)
+            .then((res) => {
+                const { token, expired } = res.data; // 取出 token, expired
+                document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`; // 將取出來的 token, expired 存到 cookie
+                window.location = 'products.html'; // 跳轉頁面至 products.html
+            })
+            .catch((err) => {
+                alert(err.response.data.message);
+            });
+        },
+    },
+}).mount('#app');
 
-const emailInput = document.querySelector('#username');
-const pwInput = document.querySelector('#password');
-const loginBtn = document.querySelector('#loginBtn');
 
-loginBtn.addEventListener('click', login);
 
-function login(){
-    const username = emailInput.value;
-    const password = pwInput.value;
+// const emailInput = document.querySelector('#username');
+// const pwInput = document.querySelector('#password');
+// const loginBtn = document.querySelector('#loginBtn');
 
-    const user = {
-        username,
-        password
-    };
-    
-    axios.post(`${url}/admin/signin`, user)
-    .then((res)=>{
-        console.log(res);
-        const { token, expired } = res.data;
-        console.log(token, expired);
-        document.cookie = `haruToken=${token}; expires=${new Date(expired)};`; // 將取出來的 token, expired 存到 cookie
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-}
+// loginBtn.addEventListener('click', login);
 
