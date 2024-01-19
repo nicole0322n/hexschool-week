53528ce429e452ed2fe3cf8f5499ffe6e40365d6
nicole0322n <nicole0322n@gamil.com>
2024-01-19 15:42:26 +0800

    week3建置、區分style.css

---
+++ week3/login.js
@@ -0,0 +1,30 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+ 
+createApp({
+    data(){
+        return{
+            user :{
+                username: '',
+                password: '',
+            },
+        }
+    },
+    methods:{
+        // 按下登入，axios 驗證 帳密資料 -> 確認無誤跳轉至商品頁面
+        login(){
+            const url = 'https://vue3-course-api.hexschool.io/v2'; // 加入站點         
+            const path = 'haru'; // 加入個人 API Path
+            
+            axios
+            .post(`${url}/admin/signin`, this.user)
+            .then((res) => {
+                const { token, expired } = res.data; // 取出 token(登入驗證), expired(時間戳記)
+                document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`; // 將取出來的 token, expired 存到 cookie
+                window.location = 'products.html'; // 跳轉頁面至 products.html
+            })
+            .catch((err) => {
+                alert(err.data.message);
+            });
+        },
+    },
+}).mount('#app');
---
+++ week3/products.js
@@ -0,0 +1,49 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+
+createApp({
+    data(){
+      return{
+        url: 'https://vue3-course-api.hexschool.io/v2',
+        path: 'haru',
+        products: [], // 存放產品資料 空陣列
+        productDetail:{}, // 暫存產品明細資料 空物件
+      }
+    },
+    methods:{
+        // 1 確認是否登入
+        checkAdmin(){
+            axios
+            .post(`${this.url}/api/user/check`)
+            .then((res) => {
+                alert('登入驗證成功！')
+                this.getProducts();  // 成功就取得產品資訊
+            })
+            .catch((err) => {
+                alert(err.data.message);
+                window.location = 'login.html'; // 錯誤跳回登入頁面
+            })
+        },
+
+        // 2 取得產品資訊
+        getProducts(){
+            axios
+            .get(`${this.url}/api/${this.path}/admin/products`)
+              .then((res) => {
+                this.products = res.data.products;
+              })
+              .catch((err) => {
+                alert(err.data.message);
+              })
+        },
+        selectItem(item){
+        this.productDetail = item ;
+      },
+    },
+    mounted(){
+        // 從 cookie 裡取得 Token（Token 僅需要設定一次）
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        // 下次進到這個網站，會將 Ｔoken 裡的資料傳給 cookie，就不需要再回傳驗證一次
+        axios.defaults.headers.common.Authorization = token; 
+        this.checkAdmin();
+    }
+  }).mount('#app');
